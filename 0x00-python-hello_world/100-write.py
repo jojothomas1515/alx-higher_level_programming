d5f926d71bbf966bb3abc7d58d487f76208cab03
Jojo <jojothomas1515@gmail.com>
2023-01-02 12:53:35 +0100

    write is located in the os module

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sys import exit
 from os import write
-text = f"and that piece of art is useful - Dora Korpar, 2015-10-19"
+text = f"and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 write(2, text.encode())
 exit(1)
