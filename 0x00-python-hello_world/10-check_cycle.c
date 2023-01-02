f86e6261173d49d733f3a8d02811aef893f02a2e
Jojo <jojothomas1515@gmail.com>
2023-01-02 19:26:11 +0100

    check_cycles

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -15,14 +15,15 @@ int check_cycle(listint_t *list)
 
 	n = list;
 	t = list->next;
+
 	for (; n && t; t = t->next, n = n->next->next)
 	{
 		if (n == n->next)
 			return (1);
 		if (n == t)
 			return (1);
-
-
+		if (n = t->next)
+			return (1);
 	}
 	return (0);
 }
