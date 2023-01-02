5aad07befea96a49acf7b896cdb27f02a90ecfb3
Jojo <jojothomas1515@gmail.com>
2023-01-02 19:11:49 +0100

    check_cycles

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -18,12 +18,12 @@ int check_cycle(listint_t *list)
 	while (n_node && t_node)
 	{
 
-		if (n_node == n_node->next)
+		if (n_node == n_node->next || n_node == n_node->next->next)
 			return (1);
 
 		if (n_node == t_node || n_node == t_node->next)
 			return (1);
-		
+
 		n_node = n_node->next;
 		t_node = t_node->next->next;
 	}
