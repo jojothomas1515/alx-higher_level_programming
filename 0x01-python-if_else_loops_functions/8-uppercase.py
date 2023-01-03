17552ec39c8b7b5dd289d433f887140497f36eb5
Jojo <jojothomas1515@gmail.com>
2023-01-03 22:55:07 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 0x61 and ord(str[i]) <= 0x7a:
+            print("{}".format(chr(ord(str[i])-0x20)), end='')
+            continue
+        print(str[i], end='')
+
+uppercase("jojo thoMaS")
