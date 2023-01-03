bf04e124bb3e932b8fb19a4220301dca58fb757d
Jojo <jojothomas1515@gmail.com>
2023-01-03 23:15:11 +0100

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 0x61 and ord(str[i]) <= 0x7a:
-            print("{}".format(chr(ord(str[i])-0x20)),
-                  end='{}'.format("" if i+1 < len(str) else "\n"))
-            continue
-        print(str[i], end='{}'.format("" if i+1 < len(str) else "\n"))
+        print("{}".format(chr(ord(str[i])-0x20)
+                          if ord(str[i]) >= 0x61 and ord(str[i]) <= 0x7a
+                          else chr(ord(str[i]))),
+              end="")
+    print("")
+
+uppercase("holberton")
