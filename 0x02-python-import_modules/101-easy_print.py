6cfa95149561a995000ea975352b9a79a753955e
Jojo Thomas <jojothomas1515@gmail.com>
2023-01-05 04:12:57 -0800

    ammends

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from os import write
-    t = "#pythoniscool\n".encode()
-    write(0,t)
+from os import write
+write(0, "#pythoniscool\n".encode())
