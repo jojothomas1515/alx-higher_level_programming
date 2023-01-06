6ab8dc2672bf4f8917ca31a529e64d95d3c6d475
Jojo Thomas <jojothomas1515@gmail.com>
2023-01-06 01:12:49 -0800

    nice

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,6 +8,8 @@ def replace_in_list(my_list, idx, element):
 
 if __name__ == '__main__':
     my_list = [x + 1 for x in range(10)]
+    this_list = my_list.copy()
     print(my_list)
     replace_in_list(my_list, 3, 22)
     print(my_list)
+    print(this_list)
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    temp_list = my_string.split()
+    text = temp_list.join()
+    print(text)
+
+
+if __name__ == '__main__':
+    text = "call crack cin"
+    print(no_c(text))
