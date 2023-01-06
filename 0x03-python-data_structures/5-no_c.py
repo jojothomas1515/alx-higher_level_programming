870219ea506c9d0026bc5500b7f90fec4dda58f0
Jojo <jojothomas1515@gmail.com>
2023-01-06 10:33:29 +0100

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    temp_list = my_string.split()
-    text = temp_list.join()
-    print(text)
+def no_c(my_string: str):
+    new_str = ""
+    for c in my_string:
+        if c in 'cC':
+            continue
+        new_str = new_str + c
+
+    return (new_str)
 
 
 if __name__ == '__main__':
-    text = "call crack cin"
+    text = "call craCCck cin"
     print(no_c(text))
