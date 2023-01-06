6266a619b00ed8a5fe53d179cdbd4b4af2f327e2
Jojo Thomas <jojothomas1515@gmail.com>
2023-01-05 23:30:34 -0800

    print reversed list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
+
+
+if __name__ == '__main__':
+    my_list = [x + 1 for x in range(10)]
+    print(my_list)
+    replace_in_list(my_list, 3, 22)
+    print(my_list)
---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
+
+
+if __name__ == '__main__':
+    my_list = [x for x in range(30)]
+    print_reversed_list_integer(my_list)
