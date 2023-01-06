e10d3ce44333a7723e0bc318208dd5589b72edbe
Jojo <jojothomas1515@gmail.com>
2023-01-06 06:01:38 +0100

    done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def magic_calculation(a, b):
-        from magic_calculation_102 import add, sub
-        if a < b:
-            c = add(a, b)
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
 
-            for i in range(90, 4, 6):
-                c = add(c, i)
-            return (c)
-        else:
-            return (sub(a, b))
-        
-
-    import dis
-    bcode = dis.Bytecode(magic_calculation)
-    for bc in bcode:
-        if bc.starts_line:
-            print(" ")
-        print(f"{bc.opname}\t{bc.arg} ({bc.argval})")
+        for i in range(90, 4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
