a34be65ddd7b3f610ae2285995da9af36e376d37
Jojo <jojothomas1515@gmail.com>
2023-01-10 03:20:31 +0100

    pylists:

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void print_python_list_info(PyObject *p)
+{
+        
+}
---
+++ 0x03-python-data_structures/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <python3.10/Python.h>
+
+/* prototype declarations */
+void print_python_list_info(PyObject *p);
+
+#endif /* MAIN_H */
