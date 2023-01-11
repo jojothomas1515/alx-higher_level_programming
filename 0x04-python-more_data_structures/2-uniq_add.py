b8d975f9b0e3f21facd625540129fda2f67b1eca
Jojo <jojothomas1515@gmail.com>
2023-01-11 20:37:38 +0100

    uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    return reduce(lambda x, y: x+y, set(my_list))
+
+
+if __name__ == '__main__':
+
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    # print("Result: {:d}".format(result))
+    print(result)
