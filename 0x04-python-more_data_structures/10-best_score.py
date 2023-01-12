dffe5b38fdd9948fa91dd68b1a9a68c016ed0283
Jojo <jojothomas1515@gmail.com>
2023-01-12 10:03:58 +0100

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    key: str
+    value: int = 0
+
+    for k, v in a_dictionary.items():
+        if v > value:
+            value = v
+            key = k
+    return (key)
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
+
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))
