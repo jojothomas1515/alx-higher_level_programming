bc809befd4cb16a3928ecd4f32f24588cfda1a1a
Jojo <jojothomas1515@gmail.com>
2023-01-24 16:58:00 +0100

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Creates a square object.
+    """
+
+    def __init__(self, size: int):
+        """
+        Square object constructor
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
