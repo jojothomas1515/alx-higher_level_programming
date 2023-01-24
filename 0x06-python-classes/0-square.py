c24852be4e32d44b649007a6c69460149a35f55a
Jojo <jojothomas1515@gmail.com>
2023-01-24 16:47:14 +0100

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 class Square:
-    pass
+    """This is my documentation"""
+
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
