9cd9e7fabc384451ac0dc85f10c5bc87fd4cea2d
Jojo <jojothomas1515@gmail.com>
2023-01-24 18:08:10 +0100

    final

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,9 @@ class Square:
             raise ValueError("size must be >= 0")
         if type(position) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or type(position[2]) is not int:
+        if type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[2] < 0:
+        if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__size = size
@@ -90,9 +90,9 @@ class Square:
         """
         if type(value) is not tuple:
             raise TypeError("value must be a tuple of 2 positive integers")
-        if type(value[0]) is not int or type(value[2]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("value must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[2] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
