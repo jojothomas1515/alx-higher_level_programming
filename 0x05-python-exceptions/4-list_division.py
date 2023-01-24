a46740fecd163ec3440000d6734b46834153d8e7
Jojo <jojothomas1515@gmail.com>
2023-01-24 15:29:12 +0100

    fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(0)
 
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             new_list.append(0)
 
         except IndexError:
