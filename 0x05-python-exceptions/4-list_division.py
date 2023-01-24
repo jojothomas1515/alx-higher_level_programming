b8903e289c33ba161f6630b7fee09ec0af9e21bd
Jojo <jojothomas1515@gmail.com>
2023-01-24 15:25:16 +0100

    fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,8 +15,24 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            if list_len - 1 != i:
+            if list_len - 1 != i and (i+1) < max(
+                    len(my_list_1),
+                    len(my_list_2)):
                 new_list.append(0)
 
     return (new_list)
 
+
+if __name__ == '__main__':
+
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
