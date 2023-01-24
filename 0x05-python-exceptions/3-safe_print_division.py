a71838d105a78624acb183221b34f3ddf2763fac
Jojo <jojothomas1515@gmail.com>
2023-01-24 07:06:02 +0100

    zero division handled

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result: int
+
+    try:
+        result = a / b
+    except Exception as e:
+        result = None
+    finally:
+        print("Inside result : {}".format(result))
+    return result
+
+
+if __name__ == '__main__':
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
