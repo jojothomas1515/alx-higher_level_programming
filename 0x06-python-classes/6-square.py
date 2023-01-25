3dd342ba5a3b503d881c5d64429654cacc06036e
Jojo <jojothomas1515@gmail.com>
2023-01-25 21:25:09 +0100

    final

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""This is a module level documentation
+"""
+This is a module level documentation
 
 My checker has been failing even tho the ouput is the same,
 i hope this is the cause.
