55568991b0c560f81d0ded2f6c9121ad0b4c0698
Jojo <jojothomas1515@gmail.com>
2023-01-25 22:30:29 +0100

    solition i av tried

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 
-"""
-This is a module documentation
+"""This is a module documentation
 
 This is so fucked up
+
 """
 
+
 class Square:
+    """Square class
+
+    Use for creating Square object instance
+
+    Example:
+        square_1 : Square = Square(10,(1,1))
+
+    """
 
     def __init__(self, size: int = 0, position=(0, 0)):
+        """Square Constructor
+
+        Initializing function
+
+        Args:
+            size (int): size of the square
+            position (tuple): display position
+
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -25,15 +43,26 @@ class Square:
         self.__position = position
 
     def area(self):
+        """Square Area
 
+        Computes the area of a Square
+
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """Size of the square"""
         return (self.__size)
 
     @size.setter
     def size(self, value: int):
+        """Set the size of the square
+
+        Args:
+            value (int): the new size
+
+        """
 
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -43,6 +72,7 @@ class Square:
             self.__size = value
 
     def my_print(self):
+        """Prints the square followed by a newline"""
 
         print("\n"*self.__position[1], end="")
         for i in range(self.__size):
@@ -53,10 +83,20 @@ class Square:
 
     @property
     def position(self):
+        """Print Position"""
         return (self.__position)
 
     @position.setter
     def position(self, value: tuple[int, int]):
+        """Position Setter
+
+        Args:
+            value (tuple): new position tuple
+
+        Description:
+            Sets the new position
+
+        """
 
         if type(value) is not tuple:
             raise TypeError("value must be a tuple of 2 positive integers")
