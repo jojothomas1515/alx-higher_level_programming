d5ea5490433df62527444ec89b8b05d51651f91c
Jojo <jojothomas1515@gmail.com>
2023-01-26 05:22:14 +0100

    new try

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,7 +22,6 @@ class Square:
 
         Args:
             size : The size of the square
-            position : print position
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
