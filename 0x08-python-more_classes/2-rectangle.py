38174dd6cfca5414371ca64b5f25d3dba6bfcf1a
Jojo <jojothomas1515@gmail.com>
2023-01-30 21:11:32 +0100

    getter and setter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise (TypeError("height must be an integer"))
         elif (value < 0):
-            raise (ValueError("height must be >= 0"))
+            raise (ValueError("height must be >=0"))
 
         self.__height = value
 
