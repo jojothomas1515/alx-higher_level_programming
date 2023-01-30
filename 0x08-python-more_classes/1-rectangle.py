7e89bacc20970c0422c9e74e7c1b0a17766991cd
Jojo <jojothomas1515@gmail.com>
2023-01-30 20:34:35 +0100

    getter and setter

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""Rectangle class module"""
+
+
+class Rectangle:
+    """Empty rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Constructor of the rectangle class
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the value for width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Sets the value for width
+
+        Args:
+            value (int): new value for width
+
+        """
+        if not isinstance(value, int):
+            raise (TypeError("width must be an integer"))
+        elif (value < 0):
+            raise (ValueError("width must be >= 0"))
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the value for width"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Sets the value for height
+
+        Args:
+            value (int): new value for height
+
+        """
+        if not isinstance(value, int):
+            raise (TypeError("height must be an integer"))
+        elif (value < 0):
+            raise (ValueError("height must be >= 0"))
+
+        self.__height = value
