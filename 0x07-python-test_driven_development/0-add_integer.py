99b00165828a3c4919afe79d7938596879eb3858
Jojo <jojothomas1515@gmail.com>
2023-01-31 12:40:02 +0100

    tdd add int

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Add integer module """
+
+
+from sys import flags
+
+
+def add_integer(a, b=98):
+    """add_integer function
+
+    Args:
+        a (int): first argument
+        b (int): second argument
+
+    Return: The sum of a and b
+
+    """
+    if not isinstance(a, int | float):
+        raise (TypeError("a must be an integer"))
+    if not isinstance(b, int | float):
+        raise (TypeError("b must be an integer"))
+
+    return (int(a) + int(b))
