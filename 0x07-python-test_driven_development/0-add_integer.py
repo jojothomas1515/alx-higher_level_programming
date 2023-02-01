155d208bcdfc11465d10c0cccf1875c8ce123a3d
Jojo <jojothomas1515@gmail.com>
2023-02-01 12:46:22 +0100

    answered

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,9 +15,9 @@ def add_integer(a, b=98):
     Return: The sum of a and b
 
     """
-    if not isinstance(a, int | float):
+    if not (isinstance(a, int) or isinstance(a, float)):
         raise (TypeError("a must be an integer"))
-    if not isinstance(b, int | float):
+    if not (isinstance(b, int) or isinstance(b, float)):
         raise (TypeError("b must be an integer"))
 
     return (int(a) + int(b))
