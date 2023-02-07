2d7dd5948e6da6486f3e657bbe5261b0117a672d
Jojo <jojothomas1515@gmail.com>
2023-02-07 17:15:25 +0100

    from_json_string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,31 +5,10 @@ import json
 
 
 def from_json_string(my_str):
-    """"""
-
-    return (json.loads(my_str))
-
-
-if __name__ == '__main__':
-    s_my_list = "[1, 2, 3]"
-    my_list = from_json_string(s_my_list)
-    print(my_list)
-    print(type(my_list))
-
-    s_my_dict = """
-    {"is_active": true, "info": {"age": 36, "average": 3.14},
-    "id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
     """
-    my_dict = from_json_string(s_my_dict)
-    print(my_dict)
-    print(type(my_dict))
+    from json string
+    :param my_str: the string
+    :return: python object
+    """
 
-    try:
-        s_my_dict = """
-        {"is_active": true, 12 }
-        """
-        my_dict = from_json_string(s_my_dict)
-        print(my_dict)
-        print(type(my_dict))
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    return json.loads(my_str)
