8ce66cdf3a1137645a255f593a9c8bf75ede71ef
Jojo <jojothomas1515@gmail.com>
2023-02-07 10:34:00 +0100

    5-base_geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Base Geometry Module"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class, from while other
+    classes are to be subclassed from
+    """
+
+    pass
