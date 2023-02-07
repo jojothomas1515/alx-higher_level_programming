4ea81c2ef0d7990f34c8694be76ccc20fcbba8b0
Jojo <jojothomas1515@gmail.com>
2023-02-07 10:00:03 +0100

    is_kind_of_class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" is kind of a class"""
+
+
+def is_kind_of_class(obj: object, a_class):
+    """
+    check if object is instance of a class or
+    instance of subclass
+
+    :param obj: class instance/object
+    :param a_class: class to com
+    :return: True or False
+    """
+
+    return isinstance(obj, a_class)
