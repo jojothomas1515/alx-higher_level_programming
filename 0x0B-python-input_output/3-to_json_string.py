b1b0ec051b308fdb121985879cbffe7bc52c7609
Jojo <jojothomas1515@gmail.com>
2023-02-07 17:19:56 +0100

    to/from_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""json string"""
+import json
+
+
+def to_json_string(my_str):
+    """
+    from json string
+    :param my_str: the string
+    :return: python object
+    """
+
+    return json.dumps(my_str)
