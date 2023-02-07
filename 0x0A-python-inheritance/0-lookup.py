31fdfb2f01169b9d64a1ac6cf2fa4a1196d42e9a
Jojo <jojothomas1515@gmail.com>
2023-02-07 03:43:05 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Look Up Module"""
+
+
+def lookup(clss: object):
+    """
+    function that returns the list of available attributes and methods of an object
+
+    :param clss: this is the passed arguments
+    :return: returns a list of available arguments
+    """
+    return dir(clss)
