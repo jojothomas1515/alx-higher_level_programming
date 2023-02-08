5bd0c8d6590a3f7f66481c2035591ed695a53774
Jojo <jojothomas1515@gmail.com>
2023-02-08 14:13:39 +0100

    save_json_file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """ Load from json file """
 
-    with  open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
