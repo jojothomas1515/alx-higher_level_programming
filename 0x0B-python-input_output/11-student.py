6d96622fbb45049a7d060d71e47ced0515b886b2
Jojo <jojothomas1515@gmail.com>
2023-02-09 04:20:10 +0100

    Student_class

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -39,16 +39,5 @@ class Student:
     def reload_from_json(self, json):
         """Reloads attributes ffrom json"""
 
-        self.__dict__ = json
-
-if __name__ == '__main__':
-    student_1 = Student("John", "Doe", 23)
-    student_2 = Student("Bob", "Dylan", 27)
-
-    j_student_1 = student_1.to_json()
-    j_student_2 = student_2.to_json(['first_name', 'age'])
-    j_student_3 = student_2.to_json(['middle_name', 'age'])
-
-    print(j_student_1)
-    print(j_student_2)
-    print(j_student_3)
+        for key in json:
+            self.__dict__[key] = json[key]
