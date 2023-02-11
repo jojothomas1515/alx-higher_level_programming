f8703b92e48d7ae723d06507bdf657b9d312e7d8
Jojo <jojothomas1515@gmail.com>
2023-02-11 23:51:44 +0100

    model

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" Base Class Module"""
+
+
+class Base:
+    """
+    Base class to be inherited from
+
+    cvar:
+        __nb_objects: private class attribute that counts the
+        number of class instance
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Base class constructor
+
+        Args:
+            id (int): instance id
+        Description:
+            if id is not None, set the instance public attr id to
+            the passed id value.
+            If id i none , increment __nb_objects and set it value to id.
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
