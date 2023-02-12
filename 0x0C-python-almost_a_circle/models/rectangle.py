6ac733ad7a5c3e246b9db01ffe3d03aa81de2442
Jojo <jojothomas1515@gmail.com>
2023-02-12 15:50:20 +0100

    first_rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,8 +25,40 @@ class Rectangle(Base):
 
         @property
         def width(self):
+            """getter for the width private variable """
             return self.__width
 
         @width.setter
         def width(self, width):
+            """setter for the width private variable"""
             self.__width = width
+
+        @property
+        def height(self):
+            """getter for the height private variable"""
+            return self.__height
+
+        @height.setter
+        def height(self, height):
+            """setter for the height private variable"""
+            self.__height = height
+
+        @property
+        def x(self):
+            """getter for the private variable x"""
+            return self.__x
+
+        @x.setter
+        def x(self, x):
+            """setter for the private variable x"""
+            self.__x = x
+
+        @property
+        def y(self):
+            """getter for the private variable y"""
+            return self.__y
+
+        @y.setter
+        def y(self, y):
+            """setter for the private variable y"""
+            self.__y = y
