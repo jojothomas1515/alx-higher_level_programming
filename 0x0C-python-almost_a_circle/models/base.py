8ec5127f87b19550e6bf55b94e91167a74d97b34
Jojo <jojothomas1515@gmail.com>
2023-02-13 00:24:09 +0100

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,65 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to file"""
+        filename = "{}.json".format(cls.__name__)
+        li = []
+        for i in list_objs:
+            li.append(i.to_dictionary())
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(li))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """method returns the list of the JSON string representation
+
+        Args:
+            json_string (str): json string representation of a list
+
+        Returns:A list
+        """
+
+        if json_string is None or json_string == "":
+            return list()
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create a new class instance
+
+        Args:
+            dictionary: dictionary of needed parameters
+        Returns: a new instance
+        """
+        new: object
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 2)
+        elif cls.__name__ == "Square":
+            new = cls(1)
+
+        new.update(**dictionary)
+
+        return new
+
+    @classmethod
+    def load_from_file(cls):
+        filename: str = "{}.json".format(cls.__name__)
+
+        n_str: str = ""
+        obj_li: list = list()
+
+        try:
+            with open(filename, "r", encoding="utf-8") as f:
+                n_str = f.read()
+        except Exception as e:
+            return list()
+
+        li_dict = cls.from_json_string(n_str)
+
+        for i in li_dict:
+            obj_li.append(cls.create(**i))
+
+        return obj_li
