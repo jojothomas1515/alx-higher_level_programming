1859626767f28cead0f0d390bccaf04854b57ec5
Jojo <jojothomas1515@gmail.com>
2023-03-15 08:35:28 +0100

    fixed

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- conver db to utf-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table name CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
