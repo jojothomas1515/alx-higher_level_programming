60ef5fcfe544432a98aa3bee09d87ca1be699d68
Jojo <jojothomas1515@gmail.com>
2023-03-15 23:33:33 +0100

    0 1 2

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- state tables
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256)
+);
