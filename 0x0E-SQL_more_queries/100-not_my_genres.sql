becbafbf70cfdeaca575bcb7bbb5ec53e45f8481
Jojo <jojothomas1515@gmail.com>
2023-03-16 16:16:00 +0100

    JOIN WITH SUBQUERIES

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- all shows with at least one genre
+SELECT DISTINCT tg.name
+FROM tv_genres tg
+        INNER JOIN tv_show_genres ts ON ts.genre_id = tg.id
+        INNER JOIN tv_shows t ON t.id = ts.show_id
+WHERE tg.name NOT IN (
+                SELECT tg.name
+                FROM tv_genres tg
+                        INNER JOIN tv_show_genres ts ON ts.genre_id = tg.id
+                        INNER JOIN tv_shows t ON t.id = ts.show_id
+                WHERE t.title = 'Dexter'
+        );
