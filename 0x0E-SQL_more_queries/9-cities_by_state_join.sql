8b1af7d05bbcc428bfcb5a4bc22186a6005f8cc8
Jojo <jojothomas1515@gmail.com>
2023-03-16 14:46:42 +0100

    wow

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,8 @@
 -- city by state join
-SELECT cities.id,
-        cities.name,
-        states.name
-FROM cities c
-        INNER JOIN states s
+SELECT id,
+        `cities`.name,
+        `states`.name
+FROM `cities` c
+        INNER JOIN `states` s
         ON c.state_id = s.id
-ORDER BY cities.id ASC;
+ORDER BY `cities`.id ASC;
