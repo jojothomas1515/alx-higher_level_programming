83be6a1e76bd3f6af714c98c01d4753785c48a69
Jojo <jojothomas1515@gmail.com>
2023-03-16 20:28:19 +0100

    wow

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+# all show without all
+SELECT title
+FROM tv_shows
+WHERE title NOT IN (SELECT title
+                    FROM tv_shows
+                             INNER JOIN tv_show_genres tsg ON tv_shows.id = tsg.show_id
+                             INNER JOIN tv_genres tg ON tsg.genre_id = tg.id
+                    WHERE tg.name = 'Comedy');
