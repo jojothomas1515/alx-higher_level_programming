e4ba4c0a9335e9119680e30dd6071236661af109
Jojo <jojothomas1515@gmail.com>
2023-03-16 14:44:00 +0100

    wow

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,6 +5,6 @@ FROM cities c
 where c.state_id = (
                 SELECT id
                 FROM states s
-                where s.name = 'California'
+                WHERE s.name = 'California'
         )
 ORDER BY id ASC;
