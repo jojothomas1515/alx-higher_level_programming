96b824b0340ff48dd6dcb53edbf7de6354d4e92b
Jojo <jojothomas1515@gmail.com>
2023-03-16 20:48:43 +0100

    wow

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- ratings
-SELECT t.title, SUM(tsr.rate) as rating
+SELECT t.title, SUM(tsr.rate) AS rating
 FROM tv_shows t
          INNER JOIN tv_show_ratings tsr ON t.id = tsr.show_id
 GROUP BY t.title
