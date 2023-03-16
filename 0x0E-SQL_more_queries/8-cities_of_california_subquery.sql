7ac94bd0edd6cfe95c23feab329ca9234af05572
Jojo <jojothomas1515@gmail.com>
2023-03-16 13:49:36 +0100

    wow

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- list the  of california
 SELECT id,
         name
-FROM cities c,
-        where c.state_id = (
+FROM cities c
+where c.state_id = (
                 SELECT id
                 FROM states
                 where name = 'California'
