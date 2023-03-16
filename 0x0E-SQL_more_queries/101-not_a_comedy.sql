0ddb0b047dd3c8f9d4597c27547b5e218b7124b9
Jojo <jojothomas1515@gmail.com>
2023-03-16 20:32:08 +0100

    wow

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,8 +2,8 @@
 SELECT title
 FROM tv_shows
 WHERE title NOT IN (SELECT title
-                    FROM tv_shows
-                             INNER JOIN tv_show_genres tsg ON tv_shows.id = tsg.show_id
+                    FROM tv_shows t
+                             INNER JOIN tv_show_genres tsg ON t.id = tsg.show_id
                              INNER JOIN tv_genres tg ON tsg.genre_id = tg.id
                     WHERE tg.name = 'Comedy')
 ORDER BY title;
