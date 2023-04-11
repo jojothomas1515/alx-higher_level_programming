6927874d56fb56db729d4e1412c420398cc59686
Jojo <jojothomas1515@gmail.com>
2023-04-11 14:13:19 +0100

    standard fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const proc = require('process')
-const arg = proc.argv[2]
+const proc = require('process');
+const arg = proc.argv[2];
 
-if (arg)
-{
-    console.log(arg)
-} else
-{
-    console.log('No argument')
+if (arg) {
+  console.log(arg);
+} else {
+  console.log('No argument');
 }
