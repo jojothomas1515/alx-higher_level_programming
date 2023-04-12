4c065ecdba4038a306e0c76f5e51b9ca61ad9242
Jojo <jojothomas1515@gmail.com>
2023-04-12 20:57:54 +0100

    printchar

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,11 +9,7 @@ class Square extends Rectangle {
 
   charPrint (c) {
     for (let i = 0; i < this.height; i++) {
-      if (c === undefined) {
-        console.log('X'.repeat(this.width));
-      } else {
-        console.log(c.repeat(this.width));
-      }
+      console.log(`${c === undefined ? 'X' : c}`.repeat(this.width));
     }
   }
 }
