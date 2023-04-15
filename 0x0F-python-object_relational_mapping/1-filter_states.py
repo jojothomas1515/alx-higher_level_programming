d1955fff1f83e7f4772dde1e5bbf6a7136e18227
Jojo <jojothomas1515@gmail.com>
2023-04-15 12:35:37 +0100

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,27 +3,28 @@
 """
 show all states
 """
-
 import MySQLdb
 import sys
 
-info = sys.argv
-try:
-    conn = MySQLdb.connect(host='localhost',
-                           port=3306,
-                           user=str(info[1]),
-                           password=str(info[2]),
-                           database=str(info[3]),
-                           charset="utf8")
-except IndexError as e:
-    print(f"USAGE: {info[0]} <user> <password> <database>")
-    exit(-1)
+if __name__ == '__main__':
+    
+    info = sys.argv
+    try:
+        conn = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=str(info[1]),
+                               password=str(info[2]),
+                               database=str(info[3]),
+                               charset="utf8")
+    except IndexError as e:
+        print(f"USAGE: {info[0]} <user> <password> <database>")
+        exit(-1)
 
-c = conn.cursor()
-c.execute("SELECT * FROM states WHERE name REGEXP '^N.*' ORDER BY id")
+    c = conn.cursor()
+    c.execute("SELECT * FROM states WHERE name REGEXP '^N.*' ORDER BY id")
 
-for row in c.fetchall():
-    print(row)
+    for row in c.fetchall():
+        print(row)
 
-c.close()
-conn.close()
+    c.close()
+    conn.close()
