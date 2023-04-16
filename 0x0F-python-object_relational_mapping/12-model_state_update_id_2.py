7476939724f975e2ec526cb560fe2935c24a17a5
Jojo <jojothomas1515@gmail.com>
2023-04-16 19:34:12 +0100

    adding item to database

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Print all state."""
+
+import sys
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]))
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+        state_2 = session.query(State).filter(State.id == 2).first()
+        state_2.name = "New Mexico"
+        session.commit()
+    except IndexError:
+        print("Usage:{} <user> <password> <database>"
+              .format(sys.argv[0]))
+        exit(-1)
