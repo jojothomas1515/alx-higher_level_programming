08d7b643a2d2b0f2c769b6db52e9cc2469f14747
Jojo <jojothomas1515@gmail.com>
2023-04-16 14:45:53 +0100

    prints the first item state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Print all state."""
+
+import sys
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    try:
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                               .format(sys.argv[1],
+                                       sys.argv[2],
+                                       sys.argv[3]))
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        session = Session()
+        data = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(data.id, data.name))
+
+    except IndexError as e:
+        print("Usage:{} <user> <password> database"
+              .format(sys.argv[1]))
+        exit(-1)
