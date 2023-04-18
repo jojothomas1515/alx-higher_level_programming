2714f21f1c12d607c48aa5d92f5f2efce878fa3d
Jojo <jojothomas1515@gmail.com>
2023-04-18 02:52:52 +0100

    relationships advance task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -9,23 +9,17 @@ import sys
 
 engine = create_engine("mysql+mysqldb://{}")
 if __name__ == '__main__':
-    try:
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                               .format(sys.argv[1],
-                                       sys.argv[2],
-                                       sys.argv[3]))
-        Base.metadata.create_all(engine)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+                                                         sys.argv[2],
+                                                         sys.argv[3]))
+    Base.metadata.create_all(engine)
 
-        Session = sessionmaker()
-        Session.configure(bind=engine)
-        session = Session()
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
-        city = City(name="San Francisco",state=State(name="California"))
-        city.state.cities.append(city)
-        session.add(city)
-        session.commit()
-
-    except IndexError:
-        print("Usage:{} <user> <password> <database>"
-              .format(sys.argv[0]))
-        exit(-1)
+    city = City(name="San Francisco", state=State(name="California"))
+    city.state.cities.append(city)
+    session.add(city)
+    session.commit()
