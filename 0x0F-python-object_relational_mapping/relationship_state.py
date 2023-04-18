71f1ff0b9fd5b33a00f29962673922b02fed0d9a
Jojo <jojothomas1515@gmail.com>
2023-04-18 03:02:05 +0100

    relationships advance task

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,5 +16,4 @@ class State(Base):
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
 
-    cities = relationship("City", backref="state", cascade="all, delete",
-                        passive_deletes=True)
+    cities = relationship("City", backref="state", cascade="all, delete")
