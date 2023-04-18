2122fedc578165fff42a82b0babf65c6424fefb2
Jojo <jojothomas1515@gmail.com>
2023-04-18 02:33:33 +0100

    relationships advance task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         session = Session()
 
         city = City(name="San Francisco", state=(State(name="California")))
-        city.state.cities = city
+        city.state.cities.append(city)
         session.add(city)
         session.commit()
 
