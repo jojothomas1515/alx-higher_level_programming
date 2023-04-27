8a7f2d2b530f6fff11db5c705461f9a41063eb11
Jojo <jojothomas1515@gmail.com>
2023-04-28 00:58:52 +0100

    list all method available on the server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL and get all methods available
-curl -sI -X OPTIONS "$1" | awk 'BEGIN {FS=":";} /^Allow/ {print $2;}'
+curl -sI -X OPTIONS "$1" | awk 'BEGIN {FS=": ";} /^Allow/ {print $2;};'
