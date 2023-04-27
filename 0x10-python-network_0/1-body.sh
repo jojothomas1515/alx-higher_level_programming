e955dbc9cc3b6db6697b90f5dd249f376ee2f404
Jojo <jojothomas1515@gmail.com>
2023-04-28 00:22:59 +0100

    prints body only if response is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-if [[ `curl -s -w '%{http_code}' $1 -o /dev/null` -eq "200"  ]]; then curl -s "$1";fi
+if [[ `curl -sL -w '%{http_code}' $1 -o /dev/null` -eq "200"  ]]; then curl -sL "$1";fi
