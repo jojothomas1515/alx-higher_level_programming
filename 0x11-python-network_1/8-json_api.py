4f106bbcd543586cb90cfac5ce43ed33ba09dc4e
Jojo <jojothomas1515@gmail.com>
2023-04-29 03:58:27 +0100

    json res

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Json api task."""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        q = sys.argv[1]
+    except IndexError:
+        q = ""
+    data = requests.post(url, data={'q':q})
+    res = data.json()
+    if len(res) == 0:
+        print("No result")
+    else:
+        try:
+            print("[{}] {}"
+                  .format(res.get('id')
+                          res.get('name')))
+        except Exception:
+            print("Not a valid JSON")
