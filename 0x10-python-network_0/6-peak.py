1240d8942b803dba49e9898a6483dd5cb60dfe57
Jojo <jojothomas1515@gmail.com>
2023-04-29 08:42:58 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,12 @@
 """Peak finder."""
 
 
-def find_peak(list_of_integers:list):
+def find_peak(list_of_integers: list):
     """Return the highest number."""
-    return list_of_integers.sort()[-1]
+    res = list_of_integers
+    len_l = len(res)
+    if (len_l == 0):
+        return None
+    res.sort()
+    return res[len_l - 1]
+
