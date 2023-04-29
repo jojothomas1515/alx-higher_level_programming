1cc8077084629340e24d2c0133f382ef8da77f2e
Jojo <jojothomas1515@gmail.com>
2023-04-29 09:16:18 +0100

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Return the status code of the website
-if [[ "$#" -lt "1" ]];then echo "No arguments passed";else curl -s -w '%{response_code}' "$1" -o /dev/null; fi
+curl -s -w '%{response_code}' "$1" -o /dev/null
