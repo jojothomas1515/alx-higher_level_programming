67844a6698c33a03f0e976e60a5cae8b3cea265c
Jojo <jojothomas1515@gmail.com>
2023-04-29 05:01:03 +0100

    github auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,10 +6,8 @@ import sys
 
 if __name__ == '__main__':
 
-    url = "https://api.github.com/authorizations"
+    url = "https://api.github.com/user"
     username = sys.argv[1]
     password = sys.argv[2]
-
     data = requests.get(url, auth=(username, password))
-    res = data.content.decode()
-    print(res)
+    print(data.json().get('id'))
