e079098171ecd2aa92b434afb0ada2c42502cfb4
Jojo <jojothomas1515@gmail.com>
2023-05-23 18:57:57 +0100

    number of episodes

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+// using request to get the number of episode Wedge Antilles  appeared
+const request = require('request');
+const process = require('process');
+const argv = process.argv;
+const argLen = argv.length;
+
+if (argLen !== 3) { process.exit(-1); }
+
+const uri = argv[2];
+
+// check the characters array in each episode json object and checks if the char link is included
+function movieFilterOnChar (movie) {
+  const charLink = 'https://swapi-api.alx-tools.com/api/people/18/';
+  return movie.characters.includes(charLink);
+}
+
+// log out the number of episodes the character appeared
+function numOfMovies (err, response, body) {
+  if (err) { console.log(err); } else if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const films = data.results.filter(movieFilterOnChar);
+    console.log(films.length);
+  }
+}
+
+request(uri, numOfMovies);
